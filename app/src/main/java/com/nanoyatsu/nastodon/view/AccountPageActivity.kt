e206cf28c08c33ca06b53d592bd9b04b3881dc31
nredjap <hinanias@gmail.com>
2019-09-05 23:19:40 +0900

    update アカウントのページにヘッダーとかアイコンを置いた

--- app/src/main/java/com/nanoyatsu/nastodon/view/AccountPageActivity.kt
+++ app/src/main/java/com/nanoyatsu/nastodon/view/AccountPageActivity.kt
@@ -1,9 +1,12 @@
 package com.nanoyatsu.nastodon.view
 
 import android.os.Bundle
+import android.text.Html
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.nanoyatsu.nastodon.R
 import com.nanoyatsu.nastodon.model.Account
+import kotlinx.android.synthetic.main.activity_account_page.*
 
 class AccountPageActivity : AppCompatActivity() {
     enum class IntentKey { ACCOUNT }
@@ -13,7 +16,12 @@ class AccountPageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_account_page)
 
         val account = intent.getParcelableExtra<Account>(IntentKey.ACCOUNT.name)
+        Glide.with(this@AccountPageActivity).load(account.headerStatic).into(header)
+        Glide.with(this@AccountPageActivity).load(account.avatarStatic).circleCrop().into(avatar)
+        displayName.text = account.displayName
+        username.text = account.username
+        note.text = Html.fromHtml(account.note, Html.FROM_HTML_MODE_COMPACT)
 
-        return
+
     }
 }
