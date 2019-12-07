2ea27dc9f6b80c4481f3477ec02e317ec7419978
nredjap <hinanias@gmail.com>
2019-12-08 02:31:14 +0900

    update ナビゲーション化する ひとまずActivityを追加

---
+++ app/src/main/java/com/nanoyatsu/nastodon/view/NavHostActivity.kt
@@ -0,0 +1,13 @@
+package com.nanoyatsu.nastodon.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nanoyatsu.nastodon.R
+
+class NavHostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_host)
+    }
+}
