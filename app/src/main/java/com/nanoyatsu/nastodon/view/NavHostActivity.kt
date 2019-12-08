921aa180f7df6e52d80fab2f50bed91b431ed71c
nredjap <hinanias@gmail.com>
2019-12-08 18:11:05 +0900

    update navigation DrawerLayoutとActionBarを追加 / todo fix TimelineFrameFragment

--- app/src/main/java/com/nanoyatsu/nastodon/view/NavHostActivity.kt
+++ app/src/main/java/com/nanoyatsu/nastodon/view/NavHostActivity.kt
@@ -2,12 +2,30 @@ package com.nanoyatsu.nastodon.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.nanoyatsu.nastodon.R
+import com.nanoyatsu.nastodon.databinding.ActivityNavHostBinding
 
 class NavHostActivity : AppCompatActivity() {
+    lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nav_host)
+        val binding =
+            DataBindingUtil.setContentView<ActivityNavHostBinding>(this, R.layout.activity_nav_host)
+        drawerLayout = binding.drawerLayout
+
+        val navController = findNavController(R.id.main_fragment_container)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.main_fragment_container)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
 }
