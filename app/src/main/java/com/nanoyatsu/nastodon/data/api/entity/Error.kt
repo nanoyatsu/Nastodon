15052e5d1c5fcce10e344f8075e7a5e712d9b4f7
nredjap <hinanias@gmail.com>
2020-01-11 16:32:02 +0900

    update API_Entity Error定義

---
+++ app/src/main/java/com/nanoyatsu/nastodon/data/api/entity/Error.kt
@@ -0,0 +1,29 @@
+package com.nanoyatsu.nastodon.data.api.entity
+
+// The most important part of an error response is the HTTP status code.
+// 「レスポンスのHTTPステータスコードみたほうがいい」
+data class Error(
+    val error: String,
+    val errorDescription: String?
+)
+
+// 401 - Unauthorized
+//  require_authenticated_user!
+//   Error: This API requires an authenticated user. Appears when the instance is in secure mode, which disables all public use of API methods.
+// 403 - Forbidden
+//  current_user.disabled?
+//   Error: Your login is currently disabled. Appears when the OAuth token's authorized user has had their account disabled by a moderator.
+//  !current_user.confirmed?
+//   Error: Your login is missing a confirmed e-mail address. Appears when the email address associated with the OAuth token's authorized user's account has not yet been confirmed.
+//  !current_user.approved?
+//   Error: Your login is currently pending approval. Appears when the OAuth token's authorized user has signed up on an instance with approval-required registrations, and the user has not yet had their account approved by a moderator.
+// 404 - Not Found
+//  RecordNotFound
+//   Error: Record not found. Appears when an entity record does not exist, or the authorized user is not within the audience of a private entity.operates on a user.
+// 422 - Unprocessable Entity
+//  RecordInvalid
+//   Error: {string}. May appear when entity creation failed.
+//  RecordNotUnique
+//   Error: Duplicate record. Appears when you are trying to pin an account or status that is already pinned.
+//  !current_user
+//   Error: This method requires an authenticated user. Appears when using an OAuth token without an authorized user (or no token at all), while trying to call an API method that requires a user to be processed.
