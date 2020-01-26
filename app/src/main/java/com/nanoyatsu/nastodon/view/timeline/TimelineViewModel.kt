8bf51de9012a71857ad66dc2708f2c1ede0b0cf3
nredjap <hinanias@gmail.com>
2020-01-26 23:05:10 +0900

    update repositoryパターン化 TimelineDataSource関連削除

--- app/src/main/java/com/nanoyatsu/nastodon/view/timeline/TimelineViewModel.kt
+++ app/src/main/java/com/nanoyatsu/nastodon/view/timeline/TimelineViewModel.kt
@@ -1,17 +1,11 @@
 package com.nanoyatsu.nastodon.view.timeline
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations.switchMap
 import androidx.lifecycle.ViewModel
-import androidx.paging.LivePagedListBuilder
-import androidx.paging.PagedList
-import com.nanoyatsu.nastodon.components.networkState.NetworkState
 import com.nanoyatsu.nastodon.data.api.MastodonApiManager
 import com.nanoyatsu.nastodon.data.api.endpoint.MastodonApiTimelines
 import com.nanoyatsu.nastodon.data.api.entity.APIStatus
 import com.nanoyatsu.nastodon.data.database.dao.TimelineDao
 import com.nanoyatsu.nastodon.data.database.entity.AuthInfo
-import com.nanoyatsu.nastodon.data.entity.Status
 import com.nanoyatsu.nastodon.data.repository.timeline.TimelineRepository
 import retrofit2.Response
 
@@ -36,22 +30,6 @@ class TimelineViewModel(
     val isInitialising = repoResult.refreshState
     private val refresh = repoResult.refresh
     private val retry = repoResult.retry
-
-    val _statuses: LiveData<PagedList<Status>>
-    val _networkState: LiveData<NetworkState>
-    val _isInitialising: LiveData<Boolean>
-    private val _refresh: () -> Unit
-    private val _retry: () -> Unit
-
-    init {
-        val sourceFactory =
-            TimelineDataSourceFactory(kind, apiManager.timelines, auth.accessToken)
-        _statuses = LivePagedListBuilder<String, Status>(sourceFactory, TIMELINE_PAGE_SIZE).build()
-        _networkState = switchMap(sourceFactory.sourceLiveData) { it.networkState }
-        _isInitialising = switchMap(sourceFactory.sourceLiveData) { it.isInitialising }
-        _refresh = { sourceFactory.sourceLiveData.value?.invalidate() }
-        _retry = { sourceFactory.sourceLiveData.value?.retryAllFailed() }
-    }
 
     fun refreshTimeline() = refresh.invoke()
     fun retry() = retry.invoke()
