2d98d845158b28480af49206e0ae22784f7c9a07
nredjap <hinanias@gmail.com>
2020-02-16 21:14:15 +0900

    refactor DI コンテナによって不要になったNoticeFragmentの依存を削除

--- app/src/main/java/com/nanoyatsu/nastodon/view/notice/NoticeFragment.kt
+++ app/src/main/java/com/nanoyatsu/nastodon/view/notice/NoticeFragment.kt
@@ -1,7 +1,6 @@
 package com.nanoyatsu.nastodon.view.notice
 
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,33 +11,17 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.nanoyatsu.nastodon.NastodonApplication
-import com.nanoyatsu.nastodon.data.api.MastodonApiManager
-import com.nanoyatsu.nastodon.data.database.dao.NoticeDao
-import com.nanoyatsu.nastodon.data.database.entity.AuthInfo
 import com.nanoyatsu.nastodon.data.domain.Account
 import com.nanoyatsu.nastodon.data.domain.Attachment
 import com.nanoyatsu.nastodon.data.domain.Status
 import com.nanoyatsu.nastodon.databinding.FragmentNoticeBinding
 import com.nanoyatsu.nastodon.view.timeline.TimelineItemViewHolder
 import kotlinx.android.synthetic.main.activity_nav_host.*
-import javax.inject.Inject
 
 class NoticeFragment : Fragment() {
 
     private lateinit var binding: FragmentNoticeBinding
     lateinit var kind: NoticeViewModel.Kind
-
-    @Inject
-    lateinit var auth: AuthInfo
-    @Inject
-    lateinit var noticeDao: NoticeDao
-    @Inject
-    lateinit var apiManager: MastodonApiManager
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        (activity!!.application as NastodonApplication).appComponent.inject(this)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
